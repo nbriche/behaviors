a0005ca4c84fae0f14178f96ea268f4023890c04
remi <remicollet@users.noreply.github.com>
2011-10-05 14:01:23 +0000

    [0.80] fix solution type mandatory, fixed #3151

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -153,12 +153,15 @@ class PluginBehaviorsTicket {
       // Wand to solve/close the ticket
       if ((isset($ticket->input['ticketsolutiontypes_id'])
              &&  $ticket->input['ticketsolutiontypes_id'])
+          || (isset($ticket->input['solution'])
+              &&    $ticket->input['solution'])
           || (isset($ticket->input['status'])
-             && in_array($ticket->input['status'], array('solved','closed')))) {
+              && in_array($ticket->input['status'], array('solved', 'closed')))) {
 
          if ($config->getField('is_ticketrealtime_mandatory')) {
             if (!$dur) {
                unset($ticket->input['status']);
+               unset($ticket->input['solution']);
                unset($ticket->input['ticketsolutiontypes_id']);
                addMessageAfterRedirect($LANG['plugin_behaviors'][101], true, ERROR);
             }
@@ -166,6 +169,8 @@ class PluginBehaviorsTicket {
          if ($config->getField('is_ticketsolutiontype_mandatory')) {
             if (!$sol) {
                unset($ticket->input['status']);
+               unset($ticket->input['solution']);
+               unset($ticket->input['ticketsolutiontypes_id']);
                addMessageAfterRedirect($LANG['plugin_behaviors'][100], true, ERROR);
             }
          }
