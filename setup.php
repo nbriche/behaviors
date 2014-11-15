3d1f565e924afac1b29d4f790db039bfc28fc41c
yllen <yllen@users.noreply.github.com>
2014-11-15 17:09:06 +0000

    create tag 0.84.3

--- setup.php
+++ setup.php
@@ -22,7 +22,7 @@
 
  @package   behaviors
  @author    Remi Collet
- @copyright Copyright (c) 2010-2013 Behaviors plugin team
+ @copyright Copyright (c) 2010-2014 Behaviors plugin team
  @license   AGPL License 3.0 or (at your option) any later version
             http://www.gnu.org/licenses/agpl-3.0-standalone.html
  @link      https://forge.indepnet.net/projects/behaviors
@@ -71,7 +71,8 @@ function plugin_init_behaviors() {
    );
 
    $PLUGIN_HOOKS['pre_item_update']['behaviors'] = array(
-      'Ticket'    => array('PluginBehaviorsTicket',   'beforeUpdate')
+      'Problem'    => array('PluginBehaviorsProblem', 'beforeUpdate'),
+      'Ticket'     => array('PluginBehaviorsTicket',  'beforeUpdate')
    );
 
    $PLUGIN_HOOKS['pre_item_purge']['behaviors'] = array(
@@ -95,7 +96,7 @@ function plugin_init_behaviors() {
 function plugin_version_behaviors() {
 
    return array('name'           => __('Behaviours', 'behaviors'),
-                'version'        => '0.84',
+                'version'        => '0.84.3',
                 'license'        => 'AGPLv3+',
                 'author'         => 'Remi Collet, Nelly Mahu-Lasson',
                 'homepage'       => 'https://forge.indepnet.net/projects/behaviors',
